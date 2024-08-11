e1d4387d310f1a9f41e9f65087a0315d746faf59
Admin <Admin@DESKTOP-91Q46ME>
2024-08-11 14:56:46 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyDotNetCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
